f20ef13eabc43e691b795508c283517e4cc3c13f
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-11-22 22:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let usernameInput = document.getElementById("username");
+let ex_user = document.getElementById("existing");
+ex_user.addEventListener("click", () => {
+	alert(`Logged in as ${username}`);
+});
+let rememberMeCheckbox = document.getElementById("checkbox");
+let sub_btn = document.getElementById("submit");
+ sub_btn.addEventListener("click", (event) => {
+                event.preventDefault(); 
+                if (rememberMeCheckbox.checked) {
+                    // Save details in localStorage
+                    localStorage.setItem("username", username);
+                    localStorage.setItem("password", password);
+                } else {
+                    // Remove details from localStorage
+                    localStorage.removeItem("username");
+                    localStorage.removeItem("password");
+                }
+
+                alert(`Logged in as ${username}`);
